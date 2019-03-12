7cd1d186d165fce8089fca3c7be2a141972f93a9
BuffaloRob <peckham.rob@gmail.com>
2019-03-12 15:01:56 -0400

    basic Items Controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,29 @@
 class Api::V1::ItemsController < ApplicationController
+  
+  def index
+    items = get_current_user.items.all
+
+    render json: items
+  end
+
+  def create
+    item = get_current_user.items.build(item_params)
+    item.save
+
+    render json: item
+  end
+
+  def destroy
+    item = Todo.find(params[:id])
+    item.destroy
+    
+    render json: item
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit( :name )
+  end
+
 end

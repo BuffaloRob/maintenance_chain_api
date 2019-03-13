3c4cf0a3c3c2aa7e075859d201f03bf9f8256c57
BuffaloRob <peckham.rob@gmail.com>
2019-03-13 14:17:18 -0400

    routes: namespaced routes & + routes for login & signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :users
+
+      get 'user', to: 'users#show', as: 'user_show'
+      post 'signup', to: 'users#create', as: 'user_signup'
+      post 'login', to: 'users#login', as: 'user_login'
+
+      resources :items
+
+    end
+  end
 end

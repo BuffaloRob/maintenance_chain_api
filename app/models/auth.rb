c3627c156f1f85be4b06f88ea52483e281eec1e6
BuffaloRob <peckham.rob@gmail.com>
2019-03-14 11:10:04 -0400

    Auth: fixed typos & + temp secret key

--- app/models/auth.rb
+++ app/models/auth.rb
@@ -1,18 +1,20 @@
 require 'jwt'
 
 class Auth
-  ALOGRITHM = 'HS256'
+  ALGORITHM = 'HS256'
 
   def self.encrypt(payload)
-    JWT.encode(payload, secret_key, ALOGRITHM)
+    # binding.pry
+    JWT.encode(payload, secret_key, ALGORITHM)
   end
 
   def self.decode(token)
-    JWT.decode(token, secret_key, { algorithm: ALOGRITHM }).first
+    JWT.decode(token, secret_key, { algorithm: ALGORITHM }).first
   end
 
   def self.secret_key
-    Rails.application.credentials.secret_key
+    # Rails.application.credentials.secret_key_base
+    'jajsfjl44552jafs093'
   end
 
 end

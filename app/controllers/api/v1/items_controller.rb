41515399b93f0bdcafed42cfddebe455fabde2e8
BuffaloRob <peckham.rob@gmail.com>
2019-03-20 10:57:28 -0400

    items controller: + update & set_item methods

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,26 +1,40 @@
 class Api::V1::ItemsController < ApplicationController
+  before_action :set_item, only: [:update, :destroy]
   
   def index
-    items = get_current_user.items.all
+    @items = get_current_user.items.all
     binding.pry
-    render json: items
+    render json: @items
   end
 
   def create
-    item = get_current_user.items.build(item_params)
-    item.save
+    @item = get_current_user.items.build(item_params)
 
-    render json: item
+    if @item.save
+      render json: @item
+    else
+      render json: @item.errors
+    end
+  end
+
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.errors
+    end
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    
-    render json: item
+    @item.destroy
+    # render json: @item
   end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit( :name, :user_id )

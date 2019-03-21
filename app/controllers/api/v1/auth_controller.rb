e0e1b42d7b78216593bea499129121c960a91847
BuffaloRob <peckham.rob@gmail.com>
2019-03-21 14:47:07 -0400

    auth controller: commented out all code and moved to user controller

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::AuthController < ApplicationController
 
-  def create
-    user = User.find_by(params[:email])
-    if user && user.authenticate(params[:password])
-      created_jwt = issue_token({id: user.id})
-      cookies.signed[:jwt] = {value: created_jwt, httponly: true, expires: 1.hour.from_now}
-      render json: {username: user.username}
-    else
-      render json: {
-        error: 'Username or Password Incorrect'
-      }, status: 404
-    end 
-  end
+  # def create
+  #   user = User.find_by(params[:email])
+  #   if user && user.authenticate(params[:password])
+  #     created_jwt = issue_token({ id: user.id })
+  #     cookies.signed[:jwt] = { value: created_jwt, httponly: true, expires: 1.hour.from_now }
+  #     render json: { currentUser: user }
+  #   else
+  #     render json: {
+  #       error: 'Username or Password Incorrect'
+  #     }, status: 404
+  #   end 
+  # end
 
   private
 

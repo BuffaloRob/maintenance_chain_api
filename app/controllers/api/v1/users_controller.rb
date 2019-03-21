020381568a90c6a457a7b2e6ab55980de28941fd
BuffaloRob <peckham.rob@gmail.com>
2019-03-21 14:48:37 -0400

    users controller: + new create & login methods, commented out old ones

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,23 +1,47 @@
 class Api::V1::UsersController < ApplicationController
 
+  # def create
+  #   user = User.create(user_params)
+  #   if user
+  #     jwt = Auth.encrypt({ user_id: user.id })
+  #     render json: { jwt: jwt, currentUser: user }
+  #   else
+  #     render json: { error: 'Sign Up has Failed' }, status: 400
+  #   end
+  # end
+
+  # def login
+  #   user = User.find_by(email: params[:user][:email])
+  #   if user && user.authenticate(params[:user][:password])
+  #     jwt = Auth.encrypt({ user_id: user.id })
+  #     render json: { jwt: jwt, currentUser: user }
+  #   else
+  #     render json: { error: 'Login has Failed' }, status: 400
+  #   end
+  # end
+
   def create
     user = User.create(user_params)
     if user
-      jwt = Auth.encrypt({ user_id: user.id })
-      render json: { jwt: jwt, currentUser: user }
+      created_jwt = issue_token({ id: user.id })
+      cookies.signed[:jwt] = { value: created_jwt, httponly: true, expires: 1.hour.from_now }
+      render json: { currentUser: user }
     else
-      render json: { error: 'Sign Up has Failed' }, status: 400
+      render json: { error: 'Sign up has failed' }, status: 400
     end
   end
 
   def login
-    user = User.find_by(email: params[:user][:email])
-    if user && user.authenticate(params[:user][:password])
-      jwt = Auth.encrypt({ user_id: user.id })
-      render json: { jwt: jwt, currentUser: user }
+    user = User.find_by(params[:email])
+    if user && user.authenticate(params[:password])
+      created_jwt = issue_token({ id: user.id })
+      cookies.signed[:jwt] = { value: created_jwt, httponly: true, expires: 1.hour.from_now }
+      render json: { currentUser: user }
     else
-      render json: { error: 'Login has Failed' }, status: 400
-    end
+      render json: {
+        error: 'Username or Password Incorrect'
+      }, status: 404
+    end 
   end
 
   def show

8c30c18ad5d393f546fb137254c32c9b209028be
BuffaloRob <peckham.rob@gmail.com>
2019-03-22 14:53:26 -0400

    app controller: changed decode_jwt to Auth.decode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,13 +7,13 @@ class ApplicationController < ActionController::API
 
   def authenticate_user
     jwt = cookies.signed[:jwt]
-    decode_jwt(jwt)
+    Auth.decode(jwt)
   end
 
   def get_current_user
     jwt = cookies.signed[:jwt]
     if jwt
-      user_info = decode_jwt(jwt)
+      user_info = Auth.decode(jwt)
       user ||= User.find(user_info['user_id'])
     end
     user

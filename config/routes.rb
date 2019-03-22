83542d31fbc71f952bdc3a7459b9ce31828dca84
BuffaloRob <peckham.rob@gmail.com>
2019-03-22 14:51:17 -0400

    routes: + logout route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get 'user', to: 'users#show', as: 'user_show'
       post 'signup', to: 'users#create', as: 'user_signup'
       post 'login', to: 'users#login', as: 'user_login'
+      delete 'logout', to: 'users#logout', as: 'user_logout'
 
       resources :items
 

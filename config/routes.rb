a975b1d98b801f8a85a542d6fea6371394447f98
BuffaloRob <peckham.rob@gmail.com>
2019-04-17 14:07:07 -0400

    routes: + resource for categories & logs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   namespace :api do
     namespace :v1 do
       resources :users
@@ -8,8 +9,9 @@ Rails.application.routes.draw do
       post 'login', to: 'users#login', as: 'user_login'
       get 'logout', to: 'users#logout', as: 'user_logout'
 
-
       resources :items
+      resources :categories
+      resources :logs
 
     end
   end

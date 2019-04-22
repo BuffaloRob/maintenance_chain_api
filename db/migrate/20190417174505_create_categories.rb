045cb5d473b9c8a30a3a69fd30c349fc616537d9
BuffaloRob <peckham.rob@gmail.com>
2019-04-22 09:08:14 -0400

    categories migration: changed reference from logs to items to match new relationship setup

--- db/migrate/20190417174505_create_categories.rb
+++ db/migrate/20190417174505_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :name
-      t.references :logs, foreign_key: true
+      t.references :items, foreign_key: true
 
       t.timestamps
     end

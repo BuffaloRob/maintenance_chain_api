f0898c4e845972c80a93eaee8779501ad2cb017d
BuffaloRob <peckham.rob@gmail.com>
2019-05-11 15:33:17 -0400

    category model: + custom category_attr=

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :item
   has_many :logs
+
+  def category_attributes=(attributes)
+    category = Category.find_by(id: attributes[:id])
+    self.category = category
+  end
 end

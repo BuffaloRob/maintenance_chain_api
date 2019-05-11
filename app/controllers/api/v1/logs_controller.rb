4197b8733d4b3a7d6c802a11461d9d0357e83dae
BuffaloRob <peckham.rob@gmail.com>
2019-05-11 15:32:27 -0400

    logs controller: now works with nested resources, changed from default provided by scaffold

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::LogsController < ApplicationController
-  before_action :set_log, only: [:show, :update, :destroy]
+  # before_action :set_log, only: [:show, :update, :destroy]
 
   # GET /logs
   def index
-    @logs = Log.all
-
+    @category = Category.find_by(id: params[:category_id])
+    @logs = @category.logs
     render json: @logs
   end
 
@@ -15,8 +15,8 @@ class Api::V1::LogsController < ApplicationController
 
   # POST /logs
   def create
-    @log = Log.new(log_params)
-
+    @item = Item.find(params[:item_id])
+    @log = @item.logs.create(log_params)
     if @log.save
       render json: @log#, status: :created, location: @log
     else
@@ -26,7 +26,10 @@ class Api::V1::LogsController < ApplicationController
 
   # PATCH/PUT /logs/1
   def update
-    if @log.update(log_params)
+    if params[:category_id]
+      item = Item.find_by(id: params[:item_id])
+      @log = item.logs.find_by(id: params[:id])
+      @log.update(log_params)
       render json: @log
     else
       render json: @log.errors#, status: :unprocessable_entity
@@ -35,14 +38,14 @@ class Api::V1::LogsController < ApplicationController
 
   # DELETE /logs/1
   def destroy
-    @log.destroy
+    Log.find(params[:id]).destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_log
-      @log = Log.find(params[:id])
-    end
+    # def set_log
+    #   @log = Log.find(params[:id])
+    # end
 
     # Only allow a trusted parameter "white list" through.
     def log_params

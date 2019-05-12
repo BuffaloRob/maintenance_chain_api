0ac097c7323d849c438c2817b72d32b5e6c4c82a
BuffaloRob <peckham.rob@gmail.com>
2019-05-12 14:22:36 -0400

    moved category_attributes= from category to log model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,4 @@ class Category < ApplicationRecord
   belongs_to :item
   has_many :logs
 
-  def category_attributes=(attributes)
-    category = Category.find_by(id: attributes[:id])
-    self.category = category
-  end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,4 +1,9 @@
 class Log < ApplicationRecord
   belongs_to :item
   belongs_to :category
+
+  def category_attributes=(attributes)
+    category = Category.find_by(id: attributes[:id])
+    self.category = category
+  end
 end

5b95c798fc781a955ab1e414495c16ccef2e79bb
BuffaloRob <peckham.rob@gmail.com>
2019-05-17 09:55:14 -0400

    logs controller: changed update to work w/o category_id & add past_due method

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -3,14 +3,15 @@ class Api::V1::LogsController < ApplicationController
 
   # GET /logs
   def index
-    # binding.pry
     @category = Category.find_by(id: params[:category_id])
     @logs = @category.logs
+    # binding.pry
     render json: @logs
   end
 
   # GET /logs/1
   def show
+    @log = Log.find(params[:id]) 
     render json: @log
   end
 
@@ -28,19 +29,37 @@ class Api::V1::LogsController < ApplicationController
 
   # PATCH/PUT /logs/1
   def update
-    if params[:category_id]
-      item = Item.find_by(id: params[:item_id])
-      @log = item.logs.find_by(id: params[:id])
-      @log.update(log_params)
+    item = Item.find_by(id: params[:item_id])
+    @log = item.logs.find_by(id: params[:id])
+    if @log.update(log_params)
       render json: @log
     else
-      render json: @log.errors#, status: :unprocessable_entity
+      render json: @log.errors
     end
+    # if params[:category_id]
+    #   item = Item.find_by(id: params[:item_id])
+    #   @log = item.logs.find_by(id: params[:id])
+    #   @log.update(log_params)
+    #   render json: @log
+    # else
+    #   render json: @log.errors#, status: :unprocessable_entity
+    # end
   end
 
   # DELETE /logs/1
   def destroy
     Log.find(params[:id]).destroy
+  end
+
+  def past_due
+    @current_user_logs = []
+    @items = get_current_user.items.all
+    @items.each do |item|
+      item.logs.past_due.each do |log|
+        @current_user_logs << log
+        render json: @current_user_logs
+      end
+    end
   end
 
   private

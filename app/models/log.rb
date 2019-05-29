6e120a68ba694d0d656c8f64f28e25e39747b125
BuffaloRob <peckham.rob@gmail.com>
2019-05-29 14:27:39 -0400

    log model & serializer: changed relationship to beongs to log, through category

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,6 +1,6 @@
 class Log < ApplicationRecord
-  belongs_to :item
   belongs_to :category
+  belongs_to :item, through: :category
 
   def category_attributes=(attributes)
     category = Category.find_by(id: attributes[:id])
--- app/serializers/log_serializer.rb
+++ app/serializers/log_serializer.rb
@@ -1,6 +1,7 @@
 class LogSerializer < ActiveModel::Serializer
   attributes :id, :notes, :tools, :cost, :date_performed, :date_due
   # has_one :items
-  belongs_to :item
   belongs_to :category
+  belongs_to :item, through: :category
+
 end

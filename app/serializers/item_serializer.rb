8b4673a9e7b81ca4a35d140cc060b110c970dc28
BuffaloRob <peckham.rob@gmail.com>
2019-05-29 14:28:26 -0400

    item model & serializer: changed relationship to has many logs, through category

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -2,6 +2,6 @@ class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name
   # has_one :user
   belongs_to :user
-  has_many :logs
   has_many :categories
+  has_many :logs, through: :categories
 end

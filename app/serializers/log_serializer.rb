a77efe38be46f57dbae6a0d54e2a1a926b894520
BuffaloRob <peckham.rob@gmail.com>
2019-06-02 15:36:46 -0400

    log serializer: + :category_id

--- app/serializers/log_serializer.rb
+++ app/serializers/log_serializer.rb
@@ -1,5 +1,5 @@
 class LogSerializer < ActiveModel::Serializer
-  attributes :id, :notes, :tools, :cost, :date_performed, :date_due
+  attributes :id, :notes, :tools, :cost, :date_performed, :date_due, :category_id
   # has_one :items
   belongs_to :category
 end

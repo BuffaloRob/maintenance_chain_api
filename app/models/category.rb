07cb246b393e0e6d526a4bd6c27a182905d74648
BuffaloRob <peckham.rob@gmail.com>
2019-06-03 09:42:54 -0400

    Item & Category models: added dependent: :destroy to model relationships

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  has_many :logs
+  has_many :logs, dependent: :destroy
   belongs_to :item
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :categories
-  has_many :logs, through: :categories
+  has_many :categories, dependent: :destroy
+  has_many :logs, through: :categories, dependent: :destroy
 end

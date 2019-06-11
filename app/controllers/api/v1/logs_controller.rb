e3e77de6e0d9281c6c5f4e025aa30ef6f960e65e
BuffaloRob <peckham.rob@gmail.com>
2019-06-11 10:57:07 -0400

    log controller: working on past_due to retrieve only the most recent log, & create to automatically change boolean on the last log made when a new log is made

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,11 +1,8 @@
 class Api::V1::LogsController < ApplicationController
-  # before_action :set_log, only: [:show, :update, :destroy]
-
   # GET /logs
   def index
     @category = Category.find_by(id: params[:category_id])
     @logs = @category.logs
-    # binding.pry
     render json: @logs
   end
 
@@ -16,6 +13,20 @@ class Api::V1::LogsController < ApplicationController
   end
 
   # POST /logs
+  def create
+  #   @category = Category.find(params[:category_id])
+  #   @last_log = @category.logs.order(date_due: :desc).first
+  #   @log = @category.logs.create(log_params)
+  #   if @log.save
+  #     @last_log.current = false
+  #     #@last_log.toggle(:current).save
+  #     #@last_log.update(active: false)
+  #     @lost_log.save
+  #     render json: @log
+  #   else
+  #     render json: @log.errors
+  #   end
+  # end
   def create
     @category = Category.find(params[:category_id])
     @log = @category.logs.create(log_params)
@@ -43,24 +54,55 @@ class Api::V1::LogsController < ApplicationController
   end
 
   def past_due
-    @current_user_logs = []
+    @all_past_due = []
     @items = get_current_user.items.all
     @items.each do |item|
-      item.logs.past_due.each do |log|
-        @current_user_logs << log
+      item.logs.past_due.current_log.each do |log|
+        @all_past_due << log
       end
     end
-    render json: @current_user_logs
+    render json: @all_past_due
+
+    ####
+    # @all_logs = []
+    # @last_performed = []
+    # @items = get_current_user.items.all
+    # @items.each do |item|
+    #   item.logs.each do |log|
+    #     @all_logs << log
+    #   end
+    # end
+    # @grouped = @all_logs.group_by{ |x| x[:category_id] }
+    # @grouped.each do |key, array|
+    #   @last_performed << array.first
+    # end
+    # @last_performed.each |log|
+    #   #move log to new array (or reject) if date_due > today
+    #   binding.pry
+    # end
+    #####
+    # @all_logs = []
+    # @last_performed = []
+    # @items = get_current_user.items.all
+    # @items.each do |item|
+    #   item.logs.past_due.each do |log|
+    #     @all_logs << log
+    #   end
+    # end
+    # @grouped = @all_logs.group_by{ |x| x[:category_id] }
+    # # logs are default scoped to order by date_due :desc thats why array.first can be used
+    # @grouped.each do |key, array|
+    #   @last_performed << array.first
+    #   # binding.pry
+    # end
+    # render json: @final_array
+    #####
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    # def set_log
-    #   @log = Log.find(params[:id])
-    # end
-
     # Only allow a trusted parameter "white list" through.
     def log_params
       params.require(:log).permit(:notes, :tools, :cost, :date_performed, :date_due, :item_id, :category_id)
     end
+
 end

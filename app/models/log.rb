fb8062c3b8036b5ba3e0200754ebf1d4f925cf52
BuffaloRob <peckham.rob@gmail.com>
2019-06-11 10:57:35 -0400

    log model: added default scop and scope

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,13 +1,18 @@
 class Log < ApplicationRecord
   belongs_to :category
 
+  default_scope { order(date_due: :desc) }
+
+  scope :current_log, ->{ where(active: true) }
+
   def category_attributes=(attributes)
     category = Category.find_by(id: attributes[:id])
     self.category = category
   end
 
   def self.past_due
-    where("date_due <=?", Time.current)
+    where("date_due <=?", Time.current).where()
+    # where("date_due <=?", Time.current).order(date_due: :desc)
   end
   
 end

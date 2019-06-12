18c8646181bf696c54aa6a2b35c14c278b0ce9a0
BuffaloRob <peckham.rob@gmail.com>
2019-06-12 11:12:32 -0400

    log model: removed current_log scope and commented out past_due methods for future reference

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,16 +3,19 @@ class Log < ApplicationRecord
 
   default_scope { order(date_due: :desc) }
 
-  scope :current_log, ->{ where(active: true) }
-
   def category_attributes=(attributes)
     category = Category.find_by(id: attributes[:id])
     self.category = category
   end
 
-  def self.past_due
-    where("date_due <=?", Time.current).where()
-    # where("date_due <=?", Time.current).order(date_due: :desc)
-  end
+  # def self.past_due
+  #   where("date_due <=?", Time.current)
+  # end
   
+  # def self.latest_past_due
+  #   self.past_due.to_a.flatten
+  #     .group_by(&:category_id)
+  #     .collect{|key,arr| arr.first}
+  # end
+
 end

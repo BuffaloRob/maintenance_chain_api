a59cb7621e3f31ab9befd982dfe2443c44c8fe67
BuffaloRob <peckham.rob@gmail.com>
2019-06-17 10:45:31 -0400

    cors: added surge client site to origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3005'
+    origins 'http://localhost:3005', 'https://maintenancechain.surge.sh'
 
     resource '*',
       headers: :any,

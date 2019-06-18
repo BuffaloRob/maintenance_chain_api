3c5a15ee2d007fbd53bf11c318b57f0d89ed81c6
BuffaloRob <peckham.rob@gmail.com>
2019-06-18 14:32:23 -0400

    App Controller: added authorized to verify token except on signin/login. Replaced get_current_user w/ current_user. Moved encrypt & decode from Auth model to here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,54 @@
 class ApplicationController < ActionController::API
-  
+  before_action :authorized
+
   def index
     render json: { message: 'successful', status: 200 }
   end
 
-  def get_current_user
-    token_array = request.headers['HTTP_AUTHORIZATION'].split(" ")
-    jwt_token = token_array[1]
-    # jwt_token = request.headers['HTTP_AUTHORIZATION']
-    if jwt_token
-      user_info = Auth.decode(jwt_token)
-      user ||= User.find(user_info['user_id'])
+  def encode_token(payload)
+    JWT.encode(payload, 'use .env secret')
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'use .env secret', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
+      end  
     end
-    user
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
   end
 
 end
+
+  # def get_current_user
+  #   token_array = request.headers['HTTP_AUTHORIZATION'].split(" ")
+  #   jwt_token = token_array[1]
+  #   # jwt_token = request.headers['HTTP_AUTHORIZATION']
+  #   if jwt_token
+  #     user_info = Auth.decode(jwt_token)
+  #     user ||= User.find(user_info['user_id'])
+  #   end
+  #   user
+  # end
+# end

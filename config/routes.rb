ea9b32754fdc206ff8776906c128e5236c9ffe66
BuffaloRob <peckham.rob@gmail.com>
2019-06-18 14:37:22 -0400

    routes: replaced users#show w/ users#profile & users#login w/ auth#create

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,11 @@ Rails.application.routes.draw do
           resources :logs
         end
       end
-
-      get 'user', to: 'users#show', as: 'user_show'
+      get 'user', to: 'users#profile', as: 'user_profile'
       post 'signup', to: 'users#create', as: 'user_signup'
-      post 'login', to: 'users#login', as: 'user_login'
+      post 'login', to: 'auth#create', as: 'user_login'
       get 'logout', to: 'users#logout', as: 'user_logout'
       get 'past_due', to: 'logs#past_due', as: 'past_due'
-
     end
   end
 

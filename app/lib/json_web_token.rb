ba3f46069d189637aad9a6c6f2867fca5ba62090
Rob Peckham <peckham.rob@gmail.com>
2020-04-16 11:58:55 -0400

    added methods to decode the user_id from the token

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,5 +1,7 @@
 # lib/json_web_token.rb
 
+#TODO: change api_audience in the credentials before deploying
+
 # frozen_string_literal: true
 require 'net/http'
 require 'uri'
@@ -17,6 +19,17 @@ class JsonWebToken
     end
   end
 
+  def self.getUser(token)
+    public_key = get_public_key
+    decoded = JWT.decode(
+      token, 
+      public_key,
+      true,
+      algorithm: 'RS256'
+    )
+    decoded[0]['sub']
+  end
+
   def self.jwks_hash
     jwks_raw = Net::HTTP.get URI("https://maintenance-chain.auth0.com/.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
@@ -31,5 +44,23 @@ class JsonWebToken
         ]
       end
     ]
+  end
+
+  # TODO: This isn't very DRY, it does the same thing as jwks_hash but just returns the public key instead of the hash with a key of 'kid' and its value is the public key
+  def self.get_public_key
+    jwks_raw = Net::HTTP.get URI("https://maintenance-chain.auth0.com/.well-known/jwks.json")
+    jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
+    hash = Hash[
+      jwks_keys
+      .map do |k|
+        [
+          k['kid'],
+          OpenSSL::X509::Certificate.new(
+            Base64.decode64(k['x5c'].first)
+          ).public_key
+        ]
+      end
+    ]
+    hash.values[0]
   end
 end
